59f4a0302aa646d0fd7f1e5bbda6b5d55ab7ef2b
Guilherme Cruz Ferreira <78061698+Guilherme-Cruz-Ferreira@users.noreply.github.com>
2021-02-09 09:36:58 -0300

    Update tic_tac_toe.py
    - no processo de criar o modo single-player
    - converti as condições de vitória para uma função com o intuito de chamá-la pelo menos duas vezes no futuro

--- projetos/tic_tac_toe.py
+++ projetos/tic_tac_toe.py
@@ -1,8 +1,7 @@
 # jogo da velha/tic tac toe
 
 # variáveis
-p1 = 0
-p2 = 0
+p1 = p2 = 0
 tabuleiro = [1, 2, 3,\
              4, 5, 6,\
              7, 8, 9]
@@ -17,8 +16,101 @@ def banner(txt, np=60):
     print('-'*np)
 
 
-def printGuia():
-    print('\n-guia do tabuleiro-')
+def selection():
+    tset = True
+    while tset: # Seleciona X ou O / presente nos 2 gms
+        selection = str(input(f'\nOpções restantes de marcadores {l}. selecionar qual?: '))[0].strip().upper()
+        for n in l:
+            if selection == n:
+                p1 = n
+                l.remove(p1)
+                for o in l:
+                    p2 = o
+                    print(f'jogador 1: {p1}\njogador 2: {p2}')
+                    tset = False
+                return p1, p2
+            else:
+                print('marcador não encontrado, favor selecionar um existente!')
+
+
+def condVitorias(Q1, Q2):
+    y = True
+    cont = 2
+    k = 0
+    cond = m = True
+    q1 = Q1
+    q2 = Q2
+    while y: # teste 
+        if cont % 2 == 0:
+            jogador_da_vez = q1
+        else:
+            jogador_da_vez = q2
+        while True:
+            n = str(input(f'por favor, digite o local onde o \033[32m{jogador_da_vez}\033[m vai ser colocado: '))
+            if n.isnumeric():
+                n = int(n)
+                break
+            else:
+                print('Favor, digitar uma posição válida!!')
+                print('As posições válidas são: ')
+                printGuia()
+                
+        if n in tabuleiro:
+            u = tabuleiro.index(n)
+            tabuleiro2.pop(u)
+            tabuleiro.pop(u)
+            tabuleiro2.insert(u, jogador_da_vez)
+            tabuleiro.insert(u, jogador_da_vez)
+            printTabuleiro2()
+            # condições vitória/empate
+            if tabuleiro[0] == tabuleiro[1] == tabuleiro[2]:
+                banner(f'\033[32mO vencedor foi o {tabuleiro[0]}!!\033[m')
+                y = False
+            elif tabuleiro[3] == tabuleiro[4] == tabuleiro[5]:
+                banner(f'\033[32mO vencedor foi o {tabuleiro[0]}!!\033[m')
+                y = False
+            elif tabuleiro[6] == tabuleiro[7] == tabuleiro[8]:
+                banner(f'\033[32mO vencedor foi o {tabuleiro[0]}!!\033[m')
+                y = False
+            elif tabuleiro[0] == tabuleiro[1] == tabuleiro[2]:
+                banner(f'\033[32mO vencedor foi o {tabuleiro[0]}!!\033[m')
+                y = False
+            elif tabuleiro[3] == tabuleiro[4] == tabuleiro[5]:
+                banner(f'\033[32mO vencedor foi o {tabuleiro[3]}!!\033[m')
+                y = False
+            elif tabuleiro[6] == tabuleiro[7] == tabuleiro[8]:
+                banner(f'\033[32mO vencedor foi o {tabuleiro[6]}!!\033[m')
+                y = False
+            elif tabuleiro[0] == tabuleiro[4] == tabuleiro[8]:
+                banner(f'\033[32mO vencedor foi o {tabuleiro[0]}!!\033[m')
+                y = False
+            elif tabuleiro[6] == tabuleiro[4] == tabuleiro[2]:
+                banner(f'\033[32mO vencedor foi o {tabuleiro[6]}!!\033[m')
+                y = False
+            for n in tabuleiro:
+                if isinstance(n, str): 
+                    k += 1
+                else:
+                    continue
+            if k == 9:
+                banner('Empate!!')
+                y = False      
+            k = 0
+            cont += 1
+        else:
+            if n in range(1, 10):
+                print('\n\033[31mlocalização já está em uso!\033[m')
+                print('localizações disponíveis:')
+                printGuia()
+            else:
+                print('\033[31mlocalização não encontrada!\033[m')
+                print('localizações disponíveis: ')
+                printGuia()
+
+
+def printGuia(pg=False):
+    if pg == True:
+        print('\n-guia do tabuleiro-')
     print(f'{tabuleiro[0]} {tabuleiro[1]} {tabuleiro[2]}'.center(19))
     print(f'{tabuleiro[3]} {tabuleiro[4]} {tabuleiro[5]}'.center(19))
     print(f'{tabuleiro[6]} {tabuleiro[7]} {tabuleiro[8]}'.center(19))
@@ -32,90 +124,25 @@ def printTabuleiro2():
 
 def Jogo():
     y = True
-    tset = True
     cont = 2
     k = 0
-    while tset: # recebe as opções dos jogadores
-        selection = str(input(f'\nOpções restantes de marcadores {l}. selecionar qual?: '))[0].strip().upper()
-        for n in l:
-            if selection == n:
-                p1 = n
-                l.remove(p1)
-                for o in l:
-                    p2 = o
-                    print(f'jogador 1: {p1}\njogador 2: {p2}')
-                    tset = False
-            else:
-                print('marcador não encontrado, favor selecionar um existente!')
-    
-    printGuia()
+    cond = True
+    while cond:
+        gm = str(input('1 - modo single-player\n2 - modo multi-player\n>>>'))
 
-    while y:
-        if cont % 2 == 0:
-            jogador_da_vez = p1
+        if gm == '1':
+            q1, q2 = selection() #q2 sempre vai ser o npc
+            printGuia(pg=True)
+            condVitorias(q1, q2)
+        elif gm == '2':
+            q1, q2 = selection() #q2 sempre vai ser o npc
+            printGuia(pg=True)
+            condVitorias(q1, q2)
         else:
-            jogador_da_vez = p2
-        while True:
-            n = str(input(f'por favor, digite o local onde o \033[32m{jogador_da_vez}\033[m vai ser colocado: '))
-            if n.isnumeric():
-                n = int(n)
-                break
-            else:
-                print('Favor, digitar uma posição válida!!')
-                print(f'As posições válidas são: {printGuia}')
-
-        if n in tabuleiro:
-                u = tabuleiro.index(n)
-                tabuleiro2.pop(u)
-                tabuleiro.pop(u)
-                tabuleiro2.insert(u, jogador_da_vez)
-                tabuleiro.insert(u, jogador_da_vez)
-                printTabuleiro2()
-                
-                if tabuleiro[0] == tabuleiro[1] == tabuleiro[2]:
-                    banner(f'\033[32mO vencedor foi o {tabuleiro[0]}!!\033[m')
-                    y = False
-                elif tabuleiro[3] == tabuleiro[4] == tabuleiro[5]:
-                    banner(f'\033[32mO vencedor foi o {tabuleiro[0]}!!\033[m')
-                    y = False
-                elif tabuleiro[6] == tabuleiro[7] == tabuleiro[8]:
-                    banner(f'\033[32mO vencedor foi o {tabuleiro[0]}!!\033[m')
-                    y = False
-                elif tabuleiro[0] == tabuleiro[1] == tabuleiro[2]:
-                    banner(f'\033[32mO vencedor foi o {tabuleiro[0]}!!\033[m')
-                    y = False
-                elif tabuleiro[3] == tabuleiro[4] == tabuleiro[5]:
-                    banner(f'\033[32mO vencedor foi o {tabuleiro[3]}!!\033[m')
-                    y = False
-                elif tabuleiro[6] == tabuleiro[7] == tabuleiro[8]:
-                    banner(f'\033[32mO vencedor foi o {tabuleiro[6]}!!\033[m')
-                    y = False
-                elif tabuleiro[0] == tabuleiro[4] == tabuleiro[8]:
-                    banner(f'\033[32mO vencedor foi o {tabuleiro[0]}!!\033[m')
-                    y = False
-                elif tabuleiro[6] == tabuleiro[4] == tabuleiro[2]:
-                    banner(f'\033[32mO vencedor foi o {tabuleiro[6]}!!\033[m')
-                    y = False
-                for n in tabuleiro:
-                    if isinstance(n, str): 
-                        k += 1
-                    else:
-                        continue
-                if k == 9:
-                    banner('Empate!!')
-                    y = False      
-                k = 0
-                cont += 1
-        else:
-            if n in range(1, 10):
-                print('\n\033[31mlocalização já está em uso!\033[m')
-                print('localizações disponíveis:')
-                printGuia()
-            else:
-                print('\033[31mlocalização não encontrada!\033[m')
-                print('localizações disponíveis: ')
-                printGuia()
-
+            print('Favor selecionar uma opção válida!')
+            continue
+        cond = False
 
 banner('Jogo da velha')
+    
 Jogo()
